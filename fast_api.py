24107302acdaa3cfa668d0a5116aca02b7ccb517
25Jburton <25jburton@gmail.com>
2025-04-20 16:09:56 -0400

    start of session combined data by driver

--- fast_api.py
+++ fast_api.py
@@ -11,12 +11,20 @@ async def root():
 async def read_item(year: int = 0 ):
     return getMeetings(int(year))
 
+@app.get("/getSessions")
+async def read_item(circuit_key: int = 0, year: int = 0 ):
+    return getSessions(int(circuit_key),int(year))
+
 @app.get("/getDrivers")
 async def read_item(session_key: int = 0 ):
     return getDrivers(int(session_key))
 
+@app.get("/getStartOfSessionDataByDriver")
+async def read_item(session_key: int = 0, driver_number: int = 0):
+    return getStartOfSessionDataByDriver(int(session_key), int(driver_number))
+
 @app.get("/getCarData")
-async def read_item(driver_number: int = 1, session_key: int = 0 ):
+async def read_item(driver_number: int = 0, session_key: int = 0 ):
     return getCarData([
         {
             'driver_number': int(driver_number),
@@ -24,12 +32,15 @@ async def read_item(driver_number: int = 1, session_key: int = 0 ):
         }
     ])
 
+
+
+
 """ 
     Order of function calls
     - Get the year (Pass to the meetings endpoint to get valid keys)
     return our valid meeting_name(GP), circuit_key, country_key
 
-    - Get the location name (meeting_name(GP) selection )
+    - Get the location name (meeting_name(GP) selection)
     (call the sessions endpoint passing country_key & year )
     return our valid session_type/session_name
     return our valid date range for the session
--- open_f1_calls.py
+++ open_f1_calls.py
@@ -1,19 +1,35 @@
 from urllib.request import urlopen
 import json
 import datetime
-
+currentYear = datetime.datetime.now().year
 # Meetings
 def getMeetings(year):
     """"
-        year=2023
+        2023 - Current
         Provides a collection of all circuits used.
         A meeting refers to a Grand Prix or testing weekend and usually includes multiple sessions
+        Utilized for populating the valid track options
     """
-    currentYear = datetime.datetime.now().year
     if year >= 2023 and year <= currentYear:
         response = urlopen(f'https://api.openf1.org/v1/meetings?year={year}')
         data = json.loads(response.read().decode('utf-8'))
         return data
+    else:
+        return 'Missing required year param or year provided out of valid range (2023-Current)'
+
+# Sessions
+def getSessions(circuit_key, year):
+    """
+        Pass the circuit_key year
+        A session refers to a distinct period of track activity during a Grand Prix or testing weekend (practice, qualifying, race)
+    """
+    if year >= 2023 and year <= currentYear:
+        if circuit_key > 0:
+            response = urlopen(f'https://api.openf1.org/v1/sessions?circuit_key={circuit_key}&year={year}')
+            data = json.loads(response.read().decode('utf-8'))
+            return data
+        else:
+            return 'Invalid circuit key passed'
     else:
         return 'Missing required year param or year provided out of valid range (2023-Current)'
 
@@ -30,6 +46,71 @@ def getDrivers(session_key):
     else:
         return 'Missing required session key param'
 
+
+
+def getStartOfSessionDataByDriver(session_key, driver_number):
+    """ 
+        Returns all valid data for the session & driver passed
+        - Provides detailed information about individual laps.
+        - Provides information about cars going through the pit lane.
+        - Provides driver positions throughout a session, including initial placement and subsequent changes.
+        - Provides information about race control (racing incidents, flags, safety car, ...).
+        - Provides a collection of radio exchanges between Formula 1 drivers and their respective teams during sessions
+        - The weather over the track, updated every minute.
+    """
+    full_data = []
+    if session_key > 0:
+        # Laps
+        """
+            Provides detailed information about individual laps.
+        """
+        response = urlopen(f'https://api.openf1.org/v1/laps?session_key={session_key}&driver_number={driver_number}')
+        lap_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Lap Data': lap_data[0]})
+
+        # Pit
+        """
+            Provides information about cars going through the pit lane.
+        """
+        response = urlopen(f'https://api.openf1.org/v1/pit?session_key={session_key}&driver_number={driver_number}')
+        pit_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Pit Data': pit_data})
+
+        # Position
+        """
+            Provides driver positions throughout a session, including initial placement and subsequent changes.
+        """
+        response = urlopen(f'https://api.openf1.org/v1/position?session_key={session_key}&driver_number={driver_number}')
+        position_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Position Data': position_data[0]})
+
+        # Race control
+        """
+            Provides information about race control (racing incidents, flags, safety car, ...).
+        """
+        response = urlopen(f'https://api.openf1.org/v1/race_control?session_key={session_key}&driver_number={driver_number}')
+        race_control_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Race Control Data': race_control_data})
+
+        # Team radio
+        """
+            Provides a collection of radio exchanges between Formula 1 drivers and their respective teams during sessions
+        """
+        response = urlopen(f'https://api.openf1.org/v1/team_radio?session_key={session_key}&driver_number={driver_number}')
+        radio_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Team Radio Data': radio_data[0]})
+
+        # Weather
+        """
+            The weather over the track, updated every minute.
+        """
+        response = urlopen(f'https://api.openf1.org/v1/weather?session_key={session_key}')
+        weather_data = json.loads(response.read().decode('utf-8'))
+        full_data.append({'Weather Data': weather_data[0]})
+
+        return full_data
+    else:
+        return 'Missing required session key param'
 
 # Car data
 def getCarData(params_list):
@@ -95,12 +176,6 @@ def getCarData(params_list):
 """
     flag driver_number date>= & date<
     Provides information about race control (racing incidents, flags, safety car, ...).
-"""
-
-# Sessions
-"""
-    country_name session_name year
-    A session refers to a distinct period of track activity during a Grand Prix or testing weekend (practice, qualifying, race)
 """
 
 # Stints
